18ee4a31d4ef9e954ab479d30a5bdb43f82d2342
petermichuncc <plarrimo@uncc.edu>
2016-11-25 23:18:11 -0500

    Made the code modular and major upgrades to file layout.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,26 @@
 'use strict';
 
 angular.module('myApp', [])
-  .controller('MovieController', function($scope, $http) {
+  .controller('MovieController', function($scope, $http){
     $scope.$watch('search', function() {
       fetch();
     });
 
     $scope.search = "Sherlock Holmes";
 
-    function fetch() {
+    function fetch(){
       $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
-        .then(function(response) {
-          $scope.details = response.data;
-        });
+      .then(function(response){ $scope.details = response.data; });
 
       $http.get("http://www.omdbapi.com/?s=" + $scope.search)
-        .then(function(response) {
-          $scope.related = response.data;
-        });
+      .then(function(response){ $scope.related = response.data; });
     }
 
-    $scope.update = function(movie) {
+    $scope.update = function(movie){
       $scope.search = movie.Title;
     };
 
-    $scope.select = function() {
+    $scope.select = function(){
       this.setSelectionRange(0, this.value.length);
     }
   });
